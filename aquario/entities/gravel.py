312e714f4d0c2fd73f62f3a756411421659c6146
Evan Schoenberger <evanjschoenberger@gmail.com>
2021-04-03 10:43:40 -0700

    parameterize gravel

--- aquario/entities/gravel.py
+++ aquario/entities/gravel.py
@@ -6,11 +6,11 @@ from .entity import Entity
 class Gravel(Entity):
     
 
-    def __init__(self):
+    def __init__(self, height, density):
         self.x = 0
-        self.y = curses.LINES - 5
+        self.y = curses.LINES - height
 
-        self.sprite = '\n'.join([''.join([('oO'+' '*9*n)[randint(0,9*n+1)] for _ in range(curses.COLS-1)]) for n in range(5)])
+        self.sprite = '\n'.join([''.join([('oO'+' '*density*n)[randint(0,density*n+1)] for _ in range(curses.COLS-1)]) for n in range(height)])
 
 
     def update(self, delta_time):
