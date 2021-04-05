7396bcd35b68242aa3c7cefbe8ebf206d87a3891
Evan Schoenberger <evanjschoenberger@gmail.com>
2021-04-05 15:33:31 -0700

    add config, reflected, load fish sprite from file

--- aquario/util.py
+++ aquario/util.py
@@ -3,3 +3,25 @@ class Vector2():
     def __init__(self, x, y):
         self.x = x
         self.y = y
+
+def reflected(s: str):
+    ''' returns the reflected version of the
+    given string (not the same as reversed)
+    '''
+    reflections = {
+        '\\': '/',
+        '/': '\\',
+        '>': '<',
+        '<': '>',
+        '[': ']',
+        ']': '[',
+        '{': '}',
+        '}': '{',
+        '(': ')',
+        ')': '(',
+    }
+    s = list(s)
+    for i in range(len(s)):
+        if s[i] in reflections:
+            s[i] = reflections[s[i]]
+    return ''.join(s[::-1]) # reversed
